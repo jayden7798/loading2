945d6c879bd964245477af5f5bf83aa63819e315
jayden7798 <jayshepherd77@gmail.com>
2025-01-24 19:44:13 +1100

    save background beams

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
 
-export function BackgroundBeams({ className }: { className?: string }) {
+export const BackgroundBeams = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("absolute inset-0 overflow-hidden", className)}>
+    <div className={cn("absolute inset-0 z-0 overflow-hidden", className)}>
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
+      <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-blue-500 opacity-20 blur-[100px]" />
     </div>
   );
-}
+};
